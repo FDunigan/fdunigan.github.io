60a09c4e75585886b2c40facd0b710087fdd0b08
Doogle <fdunigan11@gmail.com>
2018-07-23 17:45:45 -0400

    firebase changes enabling user to leave their contact information.

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,75 +1,55 @@
-$(function() {
+var config = {
+    apiKey: "AIzaSyCpO6z-mu937PZOwD6S_nNTDmcSsW68QcE",
+    authDomain: "wonderful-8035a.firebaseapp.com",
+    databaseURL: "https://wonderful-8035a.firebaseio.com",
+    projectId: "wonderful-8035a",
+    storageBucket: "wonderful-8035a.appspot.com",
+    messagingSenderId: "290153267893"
+  };
 
-  $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
-    preventSubmit: true,
-    submitError: function($form, event, errors) {
-      // additional error messages or events
-    },
-    submitSuccess: function($form, event) {
-      event.preventDefault(); // prevent default submit behaviour
-      // get values from FORM
-      var name = $("input#name").val();
-      var email = $("input#email").val();
-      var phone = $("input#phone").val();
-      var message = $("textarea#message").val();
-      var firstName = name; // For Success/Failure Message
-      // Check for white space in name for Success/Fail message
-      if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-      }
-      $this = $("#sendMessageButton");
-      $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      $.ajax({
-        url: "././mail/contact_me.php",
-        type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
-        cache: false,
-        success: function() {
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        complete: function() {
-          setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-          }, 1000);
-        }
-      });
-    },
-    filter: function() {
-      return $(this).is(":visible");
-    },
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#sendMessageButton").on("click", function(event) {
+    event.preventDefault();
+  
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    var message = $("#message").val();
+  
+    var newMessage = {
+      name: name,
+      email: email,
+      phone: phone,
+      message: message
+    };
+  
+    database.ref().push(newMessage);
+  
+    console.log(newMessage.name);
+    console.log(newMessage.email);
+    console.log(newMessage.phone);
+    console.log(newMessage.message);
+  
+    $("#name").val("");
+    $("#email").val("");
+    $("#phone").val("");
+    $("#message").val("");
   });
 
-  $("a[data-toggle=\"tab\"]").click(function(e) {
-    e.preventDefault();
-    $(this).tab("show");
-  });
-});
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    var name = childSnapshot.val().name;
+    var email = childSnapshot.val().email;
+    var phone = childSnapshot.val().phone;
+    var message = childSnapshot.val().message;
+  
+    console.log(name);
+    console.log(email);
+    console.log(phone);
+    console.log(message);
+});    
 
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-  $('#success').html('');
-});
